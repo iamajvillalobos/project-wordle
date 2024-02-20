279f00d5c3cea81466e5ec359d98dca559ca9062
AJ Villalobos <hello@ajvillalobos.com>
2024-02-20 11:35:42 +0800

    Finish exercise 1

---
+++ src/components/GuessInput/GuessInput.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function GuessInput() {
+  const [input, setInput] = React.useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log({ guess: input });
+
+    setInput("");
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={input}
+        pattern="[a-zA-Z]{5}"
+        title="5 letter word"
+        onChange={(e) => setInput(e.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default GuessInput;
